558e9d0490fab51e98735db2ed54ffd9adbb66c9
Castaño Codesal <nacc@gft.com>
2023-11-15 17:42:07 +0100

    add all app login

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { LoginRequest } from 'src/app/services/auth/loginRequest';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  loginError:string="";
+
+  loginForm=this.formBuilder.group({
+    email:['iva@gmail.com', [Validators.required, Validators.email]],
+    password:['', [Validators.required]]
+  })
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private loginService: LoginService){}
+
+  get email(){
+    return this.loginForm.controls.email;
+  }
+
+  get password(){
+    return this.loginForm.controls.password;
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      this.loginService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (userData) => {
+          console.log(userData)
+        },
+        error: (errorData) => {
+          console.log(errorData)
+          this.loginError=errorData
+        },
+        complete: () => {
+          console.info("Login está completo")
+          this.router.navigateByUrl('/inicio')
+          this.loginForm.reset()
+        }
+      })
+    }
+    else{
+      this.loginForm.markAllAsTouched()
+      alert('error al ingresar los datos')
+    }
+  }
+
+
+
+}
---
+++ src/app/shared/nav/nav.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit, OnDestroy{
+
+  userLoginOn: boolean=false
+
+  constructor(private loginService:LoginService){}
+
+  ngOnDestroy(){
+      this.loginService.currentUserData.unsubscribe();
+      this.loginService.currentUserLoginOn.unsubscribe();
+    }
+
+
+  ngOnInit(): void{
+    this.loginService.currentUserLoginOn.subscribe(
+      {
+        next:(userLoginOn)=> {
+          this.userLoginOn=userLoginOn;
+        }
+      }
+    )
+  }
+
+}
